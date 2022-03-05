7184161bbe02a75f85e0deb132c69b4a73064c7c
Ghu1iyev <tu12n4c1r@code.edu.az>
2022-03-05 11:33:19 +0400

    loop-tasks

---
+++ AP204/AP204/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AP204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 246;
+            int sum = 0;
+            while(num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            Console.WriteLine(sum);
+            
+            
+        }
+    }
+}
